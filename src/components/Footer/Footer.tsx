119b21b809027608360dd39426a6e86185bbc895
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-25 21:57:15 +0300

    fix: change animation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 
 export const Footer = () => {
   return (
-    <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+    <Box sx={{ p: 6 }} component="footer">
       <Typography variant="h6" align="center" gutterBottom>
         Created By
       </Typography>
